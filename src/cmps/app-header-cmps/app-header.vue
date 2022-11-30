3a091305612979886b070438481c58846d26b02e
Yarden Farag <yarden.farag@gmail.com>
2022-11-30 11:06:50 +0200

    !

--- src/cmps/app-header-cmps/app-header.vue
+++ src/cmps/app-header-cmps/app-header.vue
@@ -1,29 +1,17 @@
-<template>
-  <header>
-    <nav>
-      <router-link to="/">
-        <span role="img" aria-label="logo">🙏</span>
-      </router-link>
-      <router-link to="/car">Cars</router-link>
-      <router-link to="/review">Reviews</router-link>
-      <router-link to="/chat">Chat</router-link>
-      <router-link to="/login">Login / Signup</router-link>
-    </nav>
-    <section class="loggedin-user" v-if="loggedInUser">
-      <router-link :to="`/user/${loggedInUser._id}`">
-        {{ loggedInUser.fullname }}
-      </router-link>
-      <span>{{ loggedInUser.score.toLocaleString() }}</span>
-      <img :src="loggedInUser.imgUrl" />
-    </section>
-  </header>
-</template>
+<template></template>
 <script>
 export default {
-  computed: {
-    loggedInUser() {
-      return this.$store.getters.loggedinUser
-    },
-  }
-}
+  name: 'app-header',
+  components: {},
+  data() {
+    return {};
+  },
+  created() { },
+  methods: {},
+  computed: {},
+  unmounted() { },
+};
 </script>
+<style>
+
+</style>
--- src/store/store.js
+++ src/store/store.js
@@ -1,15 +1,15 @@
 import Vuex from 'vuex'
 
 import { userStore } from './modules/user.store.js'
-import { carStore } from './car.store.js'
-import { reviewStore } from './modules/review.store.js'
+// import { carStore } from './car.store.js'
+// import { reviewStore } from './modules/review.store.js'
 
 export const store = Vuex.createStore({
   strict: true,
   modules: {
     userStore,
-    carStore,
-    reviewStore
+    // carStore,
+    // reviewStore
   },
   state: {
   },
